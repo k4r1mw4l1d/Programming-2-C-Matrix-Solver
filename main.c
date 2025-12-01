9f5f7c89cbe178d3efea99e247da957fc48ccece
k4r1mw4l1d <Karimwalid9876@gmail.com>
2025-12-01 23:54:55 +0200

    Addition & Subtraction Feature

--- main.c
+++ main.c
@@ -1,6 +1,74 @@
 #include <stdio.h>
 
+void inputMatrix(int r, int c, int m[r][c]){
+    for (int i = 0; i<r; i++){
+        for (int j = 0; j<c; j++){
+            printf("Enter Element [%d][%d]: ", i+1, j+1);
+            scanf("%d", &m[i][j]);
+        }
+    }
+}
+
+void printMatrix(int r, int c, int m[r][c]){
+    for (int i = 0; i<r; i++){
+        for (int j = 0; j<c; j++){
+            printf("%4d", m[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+void additionMatrix(int r, int c, int a[r][c], int b[r][c]){
+    int result[r][c];
+    for (int i = 0; i<r; i++){
+        for (int j = 0; j<c; j++){
+            result[i][j] = a[i][j] + b[i][j];
+        }
+        printf("\n");
+    }
+    printf("Result Matrix (Addition):\n");
+    printMatrix(r, c, result);
+}
+
+void subtractionMatrix(int r, int c, int a[r][c], int b[r][c]){
+    int result[r][c];
+    for (int i = 0; i<r; i++){
+        for (int j = 0; j<c; j++){
+            result[i][j] = a[i][j] - b[i][j];
+        }
+        printf("\n");
+    }
+    printf("Result Matrix (Addition):\n");
+    printMatrix(r, c, result);
+}
+
 int main(){
-    printf("Hello World");
+    int choice;
+    printf("---------------Matrix Solver---------------\n");
+    printf("1. Addition\n");
+    printf("2. Subtraction\n");
+    printf("3. Multiplication\n");
+    printf("4. Transpose\n");
+    scanf("%d", &choice);
+
+    if (choice==1 || choice == 2){
+        int r, c;
+        printf("Enter Row & Column: ");
+        scanf("%d %d", &r, &c);
+
+        int a[r][c], b[r][c];
+
+        printf("Enter Matrix A:\n");
+        inputMatrix(r, c, a);
+        
+        printf("Enter Matrix B:\n");
+        inputMatrix(r, c, b);
+
+        if (choice == 1){
+            additionMatrix(r, c, a, b);
+        } else {
+            subtractionMatrix(r, c, a, b);
+        }
+    }
     return 0;
 }
